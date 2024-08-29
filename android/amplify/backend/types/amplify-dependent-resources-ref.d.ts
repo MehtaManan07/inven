21a06ce114a749bece7c03478d49b316fa7189a3
manan-schmooze <manan@schmoozedating.com>
2024-08-29 13:32:25 +0530

    amplify stuff

---
+++ android/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "android9ab628679ab62867": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
